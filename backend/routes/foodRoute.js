08f7394e883a316283cfc7e7fb8c22d479e832dc
Sumiya Islam Barsha <sumiyabarsha992@gmail.com>
2025-07-04 02:57:22 +0600

    trying to host mongodb data

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from 'multer';
+import foodModel from '../models/foodModel.js';
 
 const foodRouter = express.Router();
 
+/*
 //Image storage Engine
 const storage = multer.diskStorage({
     destination : "uploads",
@@ -18,10 +20,15 @@ foodRouter.post('/add',upload.single('image'),addFood);
 foodRouter.get('/list',listFood);
 foodRouter.post('/remove',removeFood);
 
-
-
-
-
+*/
+foodRouter.get('/', async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json(foods);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 
 export default foodRouter;
