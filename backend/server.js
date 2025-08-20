7055b57b4af2395f0de6e0a63a7a21efe0e46d60
Sumiya Islam Barsha <sumiyabarsha992@gmail.com>
2025-08-21 00:34:39 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true })); // For parsing form data
 
 //middlewares
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://shokuji.vercel.app']
-  credentials: true,
-}));
+app.use(cors());
 
 //db connection
 connectDB();
@@ -39,6 +36,7 @@ app.get('/', (req,res)=> {
 app.listen(port, ()=> {
    console.log(`server running on http://localhost:${port}`);
    });
+
 
 
 
