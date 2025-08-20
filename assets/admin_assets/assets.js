69780805eb8345d6ccec4adda106313ecacc55a2
Sumiya Islam Barsha <sumiyabarsha992@gmail.com>
2025-08-21 01:59:53 +0600

    Update assets.js

--- assets/admin_assets/assets.js
+++ assets/admin_assets/assets.js
@@ -14,7 +14,8 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://shokuji-backend.onrender.com'; 
-//export const url = 'http://localhost:4000'
+//export const url = 'https://shokuji-backend.onrender.com'; 
+export const url = 'http://localhost:4000'
+
 
 
